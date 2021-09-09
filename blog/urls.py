031cbb3b635ab7f5e24e94b7e63aff194f35a784
hongsoojong <84722193+hongsoojong@users.noreply.github.com>
2021-09-10 01:12:52 +0900

    Updated

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,13 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("post/1", views.post1, name="post1"),
-    path("post/2", views.post2, name="post2"),
-    path("post/3", views.post3, name="post3"),
-    path("post/4", views.post4, name="post4"),
-    path("post/5", views.post5, name="post5"),
+    path("category/c/post/1", views.post1, name="post1"),
+    path("category/c/post/2", views.post2, name="post2"),
+    path("category/c/post/3", views.post3, name="post3"),
+    path("category/basic/post/4", views.post4, name="post4"),
+    path("category/c/post/5", views.post5, name="post5"),
+    path("category/basic/", views.cate_basic, name="cate-basic"),
+    path("category/c/", views.cate_c, name="cate-c"),
+    path("category/algorithm//", views.cate_algorithm, name="cate-algorithm"),
     path("", views.all_posts, name="all-posts"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,23 +6,36 @@ def main(request):
 
 
 def all_posts(request):
-    return render(request, "blog.html")
+    return render(request, "blog/main_blog.html")
+
+
+def cate_basic(request):
+    return render(request, "blog/post_categories/cate_basic.html")
+
+
+def cate_c(request):
+    return render(request, "blog/post_categories/cate_c.html")
+
+
+def cate_algorithm(request):
+    return render(request, "blog/post_categories/cate_algorithm.html")
 
 
 def post1(request):
-    return render(request, "posts/post1.html")
+    return render(request, "blog/all_posts/post1.html")
 
 
 def post2(request):
-    return render(request, "posts/post2.html")
+    return render(request, "blog/all_posts/post2.html")
 
 
 def post3(request):
-    return render(request, "posts/post3.html")
+    return render(request, "blog/all_posts/post3.html")
 
 
 def post4(request):
-    return render(request, "posts/post4.html")
+    return render(request, "blog/all_posts/post4.html")
+
 
 def post5(request):
-    return render(request, "posts/post5.html")
+    return render(request, "blog/all_posts/post5.html")
